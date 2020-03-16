5d457ce5ea9ca97d2b4a8f8919dd61937e3f2fb1
kryptiksage <kryptiksage@gmail.com>
2020-03-16 23:02:15 +0530

    kop.sh : changed package name to argument

--- kop.sh
+++ kop.sh
@@ -4,5 +4,5 @@
 pkg=$1
 
 # Matching list
-aur_list=$(curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=flat-remix" | jq -r '.results [] .Name')
-off_list=$(curl -s -X GET "https://www.archlinux.org/packages/search/json/?q=vlc" | jq -r '.results [] .pkgname')
+aur_list=$(curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$pkg" | jq -r '.results [] .Name')
+off_list=$(curl -s -X GET "https://www.archlinux.org/packages/search/json/?q=$pkg" | jq -r '.results [] .pkgname')
