879ee3ca698a24c6472d5fea076dcff1317eaf5e
kryptiksage <kryptiksage@gmail.com>
2020-03-17 19:34:39 +0530

    kop.sh : changed arg= from arg=

--- kop.sh
+++ kop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 aur_pkg () {
-	curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$2" | jq -r '.results [] .Name'
+	curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$1" | jq -r '.results [] .Name'
 }
 
 off_pkg () {
