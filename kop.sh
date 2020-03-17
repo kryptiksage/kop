ed7d4e7c1bb084fd4bec5fa06e2417047a280134
kryptiksage <kryptiksage@gmail.com>
2020-03-17 13:22:16 +0530

    kop.sh : added functions to return package names

--- kop.sh
+++ kop.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-# Package keyword
-pkg=$1
+aur_pkg () {
+	curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$2" | jq -r '.results [] .Name'
+}
 
-# Matching list
-aur_list=$(curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$pkg" | jq -r '.results [] .Name')
-off_list=$(curl -s -X GET "https://www.archlinux.org/packages/search/json/?q=$pkg" | jq -r '.results [] .pkgname')
+off_pkg () {
+curl -s -X GET "https://www.archlinux.org/packages/search/json/?$1=$2" | jq -r '.results [] .pkgname'
+}
+
+case $1 in
+	-S)
+
+	;;
+esac
