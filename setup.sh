fc655d51d4a469e17f1a06c64eb9a7ff9d2c3671
kryptiksage <kryptiksage@gmail.com>
2020-03-20 02:36:41 +0530

    setup.sh : added execute permission to kop

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@
 mkdir $HOME/.scripts 2>/dev/null
 git clone https://github.com/kryptiksage/kop.git $HOME/.scripts/kop
 
+# Execute permission to kop
+chmod +x $HOME/.scripts/kop/kop
+
 # Installing jq
 [ $(pacman -Qq jq 2>/dev/null | wc -l) -eq 0 ] && sudo pacman -S jq
 
-# Export path of .kop
+# Export path of kop
 echo export PATH="$HOME/.scripts/kop:$PATH" >> $HOME/.zshrc
