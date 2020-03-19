7eeff490f2d780aa170d7fb2cbda85224ff52010
kryptiksage <kryptiksage@gmail.com>
2020-03-20 02:29:19 +0530

    setup.sh : changed kop scripts location from .kop to kop

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # Creating directory and cloning repo 
 mkdir $HOME/.scripts 2>/dev/null
-git clone https://github.com/kryptiksage/kop.git $HOME/.scripts/.kop
+git clone https://github.com/kryptiksage/kop.git $HOME/.scripts/kop
 
 # Installing jq
 [ $(pacman -Qq jq 2>/dev/null | wc -l) -eq 0 ] && sudo pacman -S jq
 
 # Export path of .kop
-echo export PATH="$HOME/scripts/.kop:$PATH" >> $HOME/.zshrc
+echo export PATH="$HOME/scripts/kop:$PATH" >> $HOME/.zshrc
