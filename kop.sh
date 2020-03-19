58bd89bbed616ad3fc9d45fe0fa19fa6d6a06d7b
kryptiksage <kryptiksage@gmail.com>
2020-03-19 12:50:49 +0530

    kop.sh : split jq statements

--- kop.sh
+++ kop.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 aur_pkg () {
-	curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$1" | jq -r '.results [] .Name'
+	curl -s -X GET "https://aur.archlinux.org/rpc/?v=5&type=search&arg=$1" | jq '.results []'
 }
 
 off_pkg () {
-curl -s -X GET "https://www.archlinux.org/packages/search/json/?$1=$2" | jq -r '.results [] .pkgname'
+curl -s -X GET "https://www.archlinux.org/packages/search/json/?$1=$2" | jq '.results []'
 }
 
 case $1 in
@@ -13,10 +13,10 @@ case $1 in
 	[ $# -lt 2 ] && echo "Insuffient number of packages" && exit 1
 	for(( i=2; i<=$#; i++ ))
 	do
-		package=$(off_pkg name $2)
+		package=$(off_pkg name $2 | jq -r  '.pkgname')
 		if [ -z "$package" ]
 		then
-			package=$(aur_pkg $2 | grep "^$2$")
+			package=$(aur_pkg $2 |jq -r '.Name' | grep "^$2$")
 			if [ -z "$package" ]
 			then
 				echo "Invalid package name : $package"
@@ -41,6 +41,5 @@ case $1 in
 	# TODO : code for updating AUR packages
 	;;
 	*)
-	# TODO : code for listing packages
 	;;
 esac
