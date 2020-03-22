7f7c698721dfe28d27e5cdc3db9a052d4438e969
kryptiksage <kryptiksage@gmail.com>
2020-03-22 21:42:25 +0530

    setup.sh : changed path location to scripts; stopped cloning kop.git

--- setup.sh
+++ setup.sh
@@ -1,23 +1,16 @@
 #/bin/sh/
 
 # Creating directory and cloning repo 
-mkdir  $HOME/.scripts/ 2>/dev/null
+mkdir $HOME/.scripts/ 2>/dev/null
 
 # Cloning kop to .scripts
-if [ -d $HOME/.scripts/kop ]
-then
-	cd $HOME/.scripts/kop 
-	git pull
-else
-	git clone https://github.com/kryptiksage/kop.git $HOME/.scripts/kop
-fi
-
+curl -s https://raw.githubusercontent.com/kryptiksage/kop/master/kop > $HOME/.scripts/kop
 # Execute permission to kop
-chmod +x $HOME/.scripts/kop/kop
+chmod +x $HOME/.scripts/kop
 
 # Installing jq
 [ $(pacman -Qq jq 2>/dev/null | wc -l) -eq 0 ] && sudo pacman -S jq
 
 # Export path of kop
 sed -i '/export PATH*/d' $HOME/.zshrc
-echo export PATH="$HOME/.scripts/kop:$PATH" >> $HOME/.zshrc
+echo export PATH="$HOME/.scripts/:$PATH" >> $HOME/.zshrc
