1b31117002655fa26a9dbc85b7ed9dafd6d32343
kryptiksage <kryptiksage@gmail.com>
2020-03-24 10:58:04 +0530

    setup.sh : exports path variable from ~/.zshenv

--- setup.sh
+++ setup.sh
@@ -12,5 +12,6 @@ chmod +x $HOME/.scripts/kop
 [ $(pacman -Qq jq 2>/dev/null | wc -l) -eq 0 ] && sudo pacman -S jq
 
 # Export path of kop
-sed -i '/export PATH*/d' $HOME/.zshrc
-echo export PATH="$HOME/.scripts/:$PATH" >> $HOME/.zshrc
+[ -f $HOME/.zshenv ] || touch $HOME/.zshenv && sed -i '/export PATH*/d' $HOME/.zshenv
+echo export PATH="$HOME/.scripts/:$PATH" >> $HOME/.zshenv
+source $HOME/.zshenv
